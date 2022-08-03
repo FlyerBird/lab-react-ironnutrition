c274a14872db902df9fcf771e43f620ea6ff2591
FlyerBird <99897631+FlyerBird@users.noreply.github.com>
2022-08-03 11:06:53 +0200

    it2 Done

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@
 import './App.css';
 import foods from './foods.json';
 import React, {useState} from 'react';
+import FoodBox from './components/FoodBox';
+
 
 function App() {
   const [foodList, setFoodList] = useState(foods);
 
-
   return (
     <div className="App">
+    
       <h1>Food List</h1>
       {foodList.map(food => (
         <div> 
@@ -16,6 +18,13 @@ function App() {
           <img src={food.image} alt='food elem' width={150}/>
         </div>
       ))}
+
+      <FoodBox food={ {
+      name: "Orange",
+      calories: 85,
+      image: "https://i.imgur.com/abKGOcv.jpg",
+      servings: 1
+    }} />
     </div>
   );
 }
